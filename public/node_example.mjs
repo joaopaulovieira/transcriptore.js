4c313b66d618d05b7c6162b67ea16b6cbc04fbbe
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-11-21 16:41:26 -0300

    feat(public): add Node.js usage example file

---
+++ public/node_example.mjs
@@ -0,0 +1,8 @@
+import Transcriptore from './dist/transcriptore.mjs'
+import fetch from 'node-fetch'
+
+global.fetch = fetch
+const transcriptore = new Transcriptore()
+
+
+transcriptore.fetchAndParse('https://gist.githubusercontent.com/joaopaulovieira/87f92a1fd3139ed1c811aad0dfcfc011/raw/5ff2e2cee4f54a824f9d8a57a558d14d27d60aec/dash_sample.mpd').then(text => console.log('dash_sample.mpd', text))
