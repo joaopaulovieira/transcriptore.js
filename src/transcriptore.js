f7b2c19f3a9e7e5b46de45cf664f0afd11b22471
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-11-21 16:37:03 -0300

    feat(src): create fetchAndParse method
    Helper to fetch one file and parse at the same function call.

--- src/transcriptore.js
+++ src/transcriptore.js
@@ -2,4 +2,11 @@ export default class Transcriptore {
   readBuffer(buffer) {
     return new TextDecoder().decode(buffer.value, { stream: !buffer.done })
   }
+
+  fetchAndParse(url, fetchOptions = {}) {
+    return fetch(url, fetchOptions).then(response => {
+      const stream = response.body.getReader && response.body.getReader().read() || response.buffer()
+      return stream.then(buffer => this.readBuffer(buffer.value ? buffer : { value: buffer }))
+    })
+  }
 }
